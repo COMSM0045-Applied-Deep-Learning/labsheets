abf7a1abef224a60fef206a262a29d9922e2d709
jonmun <jonny.munro@hotmail.co.uk>
2020-10-16 10:03:27 +0100

    Fix reservation in train.sh

--- lab-1-dnns/train.sh
+++ lab-1-dnns/train.sh
@@ -2,8 +2,8 @@
 srun \
     --partition gpu \
     --gres gpu:1 \
-    -A comsm0018 \
-    --reservation comsm0018-lab1 \
+    -A comsm0045 \
+    --reservation comsm0045-lab1 \
     --mem 64GB \
     -t 0-00:15 \
     python train_mnist.py
