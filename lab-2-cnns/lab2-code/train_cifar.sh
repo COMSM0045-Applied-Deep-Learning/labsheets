4bf79a1950334425e083d4a408d4cc8e1ed6a5c8
Zhifan Zhu <zhifan.zhu@bristol.ac.uk>
2022-10-10 17:15:01 +0100

    [lab 2] Adds stdout and stderr in notebook and train script

--- lab-2-cnns/lab2-code/train_cifar.sh
+++ lab-2-cnns/lab2-code/train_cifar.sh
@@ -3,6 +3,8 @@
 #SBATCH --job-name=lab2
 #SBATCH --partition=teach_gpu
 #SBATCH --nodes=1
+#SBATCH -o ./log_%j.out # STDOUT out
+#SBATCH -e ./log_%j.err # STDERR out
 #SBATCH --gres=gpu:1
 #SBATCH --time=0:20:00
 #SBATCH --mem=4GB
